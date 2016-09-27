a38c84627952e266f5ddad8bc5a4ad4aa7ca699a
Nick West <mail@nickwe.st>
2016-09-27 17:24:41 -0400

    scraping working, todo: further styling

---
+++ public/app.js
@@ -0,0 +1,49 @@
+// grab the articles as a json
+$.getJSON('/articles', function(data) {
+  for (var i = 0; i<data.length; i++){
+    $('#articles').append('<h2 data-id="' + data[i]._id + '">'+ data[i].title + '</h2>'+ '<a href="'+ data[i].link + '" target="_blank">Read the Full Article</a>');
+  }
+});
+
+
+$(document).on('click', 'p', function(){
+  $('#notes').empty();
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').append('<h2>' + data.title + '</h2>');
+      $('#notes').append('<input id="titleinput" name="title" >');
+      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
+      $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+
+      if(data.note){
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+      }
+    });
+});
+
+$(document).on('click', '#savenote', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').empty();
+    });
+
+  $('#titleinput').val("");
+  $('#bodyinput').val("");
+});
---
+++ server.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/njnews');
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+
+
+// routes
+app.get('/', function(req, res) {
+  res.send(index.html);
+});
+
+app.get('/scrape', function(req, res) {
+  request('http://observer.com/new-jersey-politics/', function(error, response, html) {
+    var $ = cheerio.load(html);
+    $('a.module-entry-full-anchor').each(function(i, element) {
+
+				var result = {};
+
+				result.title = $(this).children('h2').text();
+				result.link = $(this).attr('href');
+
+				var entry = new Article (result);
+
+				entry.save(function(err, doc) {
+				  if (err) {
+				    console.log(err);
+				  }
+				  else {
+				    console.log(doc);
+				  }
+				});
+
+
+    });
+  });
+  res.send("Scrape Complete");
+});
+
+app.get('/articles', function(req, res){
+	Article.find({}, function(err, doc){
+		if (err){
+			console.log(err);
+		}
+		else {
+			res.json(doc);
+		}
+	});
+});
+
+app.get('/articles/:id', function(req, res){
+	Article.findOne({'_id': req.params.id})
+	.populate('note')
+	.exec(function(err, doc){
+		if (err){
+			console.log(err);
+		}
+		else {
+			res.json(doc);
+		}
+	});
+});
+
+
+app.post('/articles/:id', function(req, res){
+	var newNote = new Note(req.body);
+
+	newNote.save(function(err, doc){
+		if(err){
+			console.log(err);
+		}
+		else {
+			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+			.exec(function(err, doc){
+				if (err){
+					console.log(err);
+				} else {
+					res.send(doc);
+				}
+			});
+		}
+	});
+});
+app.listen(3000, function() {
+  console.log('App running on port 3000');
+});
